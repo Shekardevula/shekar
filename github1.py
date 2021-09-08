b85c8a5e04614cc4d8f756e200b41e124f91080d
shekar <dassadc@gmail.com>
2021-09-08 20:45:00 +0530

    create diamond

---
+++ github1.py
@@ -0,0 +1,13 @@
+eor=5
+i="row"
+while i>1:
+    j=rows
+    while j>i:
+        print('',end='')
+        j-=1
+    k=1
+    while k<=i:
+        print('*',end='')
+        k+=1
+    print()
+    i-=1
